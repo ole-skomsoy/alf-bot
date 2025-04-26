8688e2d6b4fb8e660970d20a47b861b24f81f5e0
0138 <oleskomsoy@proton.me>
2025-04-26 03:00:31 +0200

    improved reactions

--- src/bots/test-bot.py
+++ src/bots/test-bot.py
@@ -5,32 +5,18 @@ import random
 from commands import *
 from helpers import *
 
-
-# how to 'repost'?
-# 1. forward
-# array of reactions, [🤭, 😂, 🤣, 😆], [🔥]
 class test_bot(disc.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user}')
-        self.daily_meme_id = 0
-        self.daily_tune_id = 0
         await self.set_default_status()
         await self.post_daily_messages()
     
     async def on_message(self, message):
-        if message.author == self.user : await self.react_to_message(message)
+        if message.author == self.user : pass
         for command in commands:
             if message.content.startswith(command):
                 await message.channel.send(commands[command])
                 break
-    
-    async def react_to_message(self, message):
-        self.log(message)
-        match message.id:
-            case self.daily_meme_id:
-                message.add_reaction(random.choice(['🤭','😂','🤣','😆']))
-            case self.daily_tune_id:
-                message.add_reaction(random.choice(['🔥','🙏','😍','😤']))
     
     async def set_default_status(self):
         await self.change_presence(
@@ -42,17 +28,17 @@ class test_bot(disc.Client):
         
         meme_channel = self.get_channel(read_secret('meme_channel'))
         meme_message = await self.get_random_message(meme_channel, 10)
-        self.log(meme_message)
-        self.log(self.daily_meme_id)
+        #self.log(meme_message)
         
         tune_channel = self.get_channel(read_secret('tune_channel'))
         tune_message = await self.get_random_message(tune_channel, 10)
-        self.log(tune_channel)
+        #self.log(tune_channel)
         
         daily_meme_message = await self.post_message(repost_channel, meme_message)
+        await self.react_to_message(daily_meme_message, ['🤭','😂','🤣','😆'])
+        
         daily_tune_message = await self.post_message(repost_channel, tune_message)
-        self.daily_meme_id = daily_meme_message.id
-        self.daily_tune_id = daily_tune_message.id
+        await self.react_to_message(daily_tune_message, ['🔥','🙏','😍','😤'])
 
     async def get_random_message(self, channel, retry_count):
         around = datetime.fromtimestamp(random.randint(
@@ -76,7 +62,10 @@ class test_bot(disc.Client):
 
     async def post_message(self, channel, message):
         return await message.forward(channel)
-        
+    
+    async def react_to_message(self, message, reactions):
+        await message.add_reaction(random.choice(reactions))
+
     def log(self, object):
         print(jsonpickle.encode(object))
         print('----------------------------------------------------------------------------')
