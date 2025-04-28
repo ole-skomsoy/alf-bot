f6a443e86cb232a30513afe6165736d3224505fd
0138 <oleskomsoy@proton.me>
2025-04-28 15:31:08 +0200

    implemented daily quote+cat message

--- src/bots/lurke_rob.py
+++ src/bots/lurke_rob.py
@@ -1,6 +1,7 @@
 import discord as disc
 from discord.ext import commands
 import datetime
+import requests
 import jsonpickle
 import random
 from helpers import *
@@ -13,6 +14,7 @@ class lurke_rob(commands.Bot):
     async def on_ready(self):
         print(f'Logged in as {self.user}')
         await self.set_default_status()
+        await self.post_random_messages(True, False, False)
     
     async def on_message(self, message):
         if message.author == self.user : pass
@@ -22,20 +24,62 @@ class lurke_rob(commands.Bot):
             activity=disc.Activity(type=disc.ActivityType.listening, name='🤫'),
             status=disc.Status.dnd)
         
-    async def post_random_messages(self, from_meme_channel, from_tune_channel):
+    async def post_random_messages(self, from_quote_api, from_tune_channel, from_meme_channel):
         repost_channel = self.get_channel(read_secret('repost_channel'))
         
-        if from_meme_channel:
-            meme_channel = self.get_channel(read_secret('meme_channel'))
-            meme_message = await self.get_random_message(meme_channel, 10)
-            daily_meme_message = await self.post_message(repost_channel, meme_message)
-            await self.react_to_message(daily_meme_message, ['🤭','😂','🤣','😆'])
-        
+        if from_quote_api:
+            quote_message = self.get_random_quote()
+            cat = self.get_random_cat()
+            daily_quote_message = await self.send_quote_message(repost_channel, quote_message, cat)
+            await self.react_to_message(daily_quote_message, ['😺','😸','😹','😻','😼','😽','🙀','😿','😾'])
+            
         if from_tune_channel:
             tune_channel = self.get_channel(read_secret('tune_channel'))
             tune_message = await self.get_random_message(tune_channel, 10)
-            daily_tune_message = await self.post_message(repost_channel, tune_message)
+            daily_tune_message = await self.forward_message(repost_channel, tune_message)
             await self.react_to_message(daily_tune_message, ['🔥','🙏','😍','😤'])
+            
+        if from_meme_channel:
+            meme_channel = self.get_channel(read_secret('meme_channel'))
+            meme_message = await self.get_random_message(meme_channel, 10)
+            daily_meme_message = await self.forward_message(repost_channel, meme_message)
+            await self.react_to_message(daily_meme_message, ['🤭','😂','🤣','😆'])
+
+    def get_random_quote(self):
+        try:
+            return requests.get(f'{read_secret('quote_api')}/random').json()[0]
+            # return {"q": "You are what you believe yourself to be.", "a": "Paulo Coelho", "h": "<blockquote>&ldquo;You are what you believe yourself to be.&rdquo; &mdash; <footer>Paulo Coelho</footer></blockquote>"}
+        except:
+            print('>>> error getting random quote')
+    
+    def get_random_cat(self):
+        try:
+            return requests.get(f'{read_secret('cat_api')}/cat?json=true').json()
+        except:
+            print('>>> error getting random cat')            
+    
+    # https://discord.com/developers/embeds?url=https%253A%252F%252Fcataas.com%252Fcat
+    async def send_quote_message(self, channel, quote, cat):
+        
+        embed = disc.Embed(title=quote['a'], description=quote['q'], type='image')
+        embed = embed.set_image(url=cat['url'])
+        return await channel.send(embed=embed)
+        
+        # embed = embed.set_thumbnail(url='https://cataas.com/cat')
+        # embed = embed.set_author(name='cat_author', url='https://cataas.com/cat')
+
+        # return await channel.send(embed={
+        #     "type": "image",
+        #     "url": "https://cataas.com/cat",
+        #     "thumbnail": {
+        #         "url": "https://cataas.com/cat",
+        #         "proxy_url": "https://images-ext-1.discordapp.net/external/aWkbTEJ28MKA4B0SP4kI4yQaMhGERKYVU4rwHsKAXbo/https/cataas.com/cat",
+        #         "width": 800,
+        #         "height": 600,
+        #         "placeholder": "1SgKFYIFpYlYiWZhiRaKt9CEJQ5J",
+        #         "placeholder_version": 1,
+        #         "flags": 0}
+        # })
 
     async def get_random_message(self, channel, retry_count):
         around = datetime.datetime.fromtimestamp(random.randint(
@@ -58,7 +102,7 @@ class lurke_rob(commands.Bot):
             retry_count -= 1
             if retry_count <= 0 : raise Exception(f'>>> Failed to get random message ({retry_count} retries left)')
 
-    async def post_message(self, channel, message):
+    async def forward_message(self, channel, message):
         return await message.forward(channel)
     
     async def react_to_message(self, message, reactions):
@@ -75,12 +119,12 @@ intents.message_content = True
 # dont bother trying to change the command prefix
 bot = lurke_rob(command_prefix='/', intents=intents)
 
-@bot.tree.command(name='lr_get_random_meme', description = "Get a random meme from the meme channel")
+@bot.tree.command(name='lr_get_random_tune', description = "Get a random tune from the tune channel")
 async def get_random_meme_command(ctx):
     await bot.post_random_messages(True, False)
     await ctx.response.send_message(content='member?', delete_after=3.0)
-    
-@bot.tree.command(name='lr_get_random_tune', description = "Get a random tune from the tune channel")
+
+@bot.tree.command(name='lr_get_random_meme', description = "Get a random meme from the meme channel")
 async def get_random_meme_command(ctx):
     await bot.post_random_messages(False, True)
     await ctx.response.send_message(content='member?', delete_after=3.0)
